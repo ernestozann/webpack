510c2efacaf160f99d5f3df23c2858e5e6ea36a8
Ernesto Martinez <ernestomm99@hotmail.com>
2021-04-19 12:31:28 -0600

    HTML en Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-
     },
     resolve: {
         extensions: ['.js']
@@ -16,9 +16,16 @@ module.exports = {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                loader: 'babel-loader'
+                    loader: 'babel-loader'
                 }
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
 }
